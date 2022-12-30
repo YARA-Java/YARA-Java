a008e35c46fd882f3491c4fb51e24eb2d174ff13
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-12-30 19:39:17 +0100

    test YARA compiler create & destroy
    https://stackoverflow.com/questions/74963497/java-foreign-api-how-to-pass-pointers-and-pointers-address

--- yara-java/src/test/java/com/virustotal/yara/YaraJavaTests.java
+++ yara-java/src/test/java/com/virustotal/yara/YaraJavaTests.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
-import static com.virustotal.yara.yara_h_1.yr_initialize;
-import static com.virustotal.yara.yara_h_1.yr_finalize;
+import static com.virustotal.yara.yara_h.C_POINTER;
+import static com.virustotal.yara.yara_h.ERROR_SUCCESS;
+import static com.virustotal.yara.yara_h_1.*;
 import static com.virustotal.yara.yara_h_2.YR_VERSION;
 
 public class YaraJavaTests {
@@ -31,6 +34,19 @@ public class YaraJavaTests {
         Assertions.assertNotNull(versionString);
         Assertions.assertFalse(versionString.isEmpty());
         Assertions.assertEquals("4.2.3", versionString);
+    }
+
+    @Test
+    public void testYaraCompilerCreateAndDestroy() {
+        try (Arena arena = Arena.openConfined()) {
+            MemorySegment compilerAddress = arena.allocate(C_POINTER); // YR_COMPILER**
+            int created = yr_compiler_create(compilerAddress);
+
+            Assertions.assertEquals(ERROR_SUCCESS(), created);
+
+            MemorySegment compiler = compilerAddress.get(C_POINTER, 0); // YR_COMPILER*
+            yr_compiler_destroy(compiler);
+        }
     }
 
     @AfterAll
